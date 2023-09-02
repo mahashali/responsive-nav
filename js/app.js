0c3432204ac1dbbeec51695960ce283ea9513cbc
mohamoud ali <103631772+mahashali@users.noreply.github.com>
2023-09-02 02:12:32 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function openSlideMenu() {
+document.getElementById('side-menu').style.width = '190px';
+document.getElementById('main').style.marginLeft = '190px';
+
+}
+function closeSlideMenu() {
+document.getElementById('side-menu').style.width = '0';
+document.getElementById('main').style.marginLeft = '0';
+
+}
